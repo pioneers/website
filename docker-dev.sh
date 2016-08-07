40622d59bcad4688e26f3bfea6578a2554bde9ec
Scott Numamoto <scott.numamoto@gmail.com>
2016-08-07 01:36:21 -0700

    Update bash script with incremental build

--- docker-dev.sh
+++ docker-dev.sh
@@ -1,4 +1,4 @@
-docker run -i --rm -p 4000:4000 -v "$PWD:/srv/jekyll" numascott/jekylldev:latest
+docker run -i --rm -p 4000:4000 -v "$PWD:/srv/jekyll" numascott/jekylldev:latest serve --incremental
 
 # Breaking it down
 
