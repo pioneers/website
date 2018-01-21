c43e848926f6aaa69c02b197ace59d98bc2a4eac
Michael Wu <wuxiaohua1011@gmail.com>
2018-01-21 11:14:26 -0800

    map added in the compete/past-participation.html section

---
+++ compete/school_source.js
@@ -0,0 +1,5 @@
+var schools = [
+  ["Acalanes High School", 37.9043022,-122.0999264],
+  ["Alameda Community Learning Center",37.7794615,-122.2894],
+  ["Alameda High School",37.7702805,-122.2859331],
+];
