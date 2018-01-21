475a83afd2c68c4ef2f36f582f8922387365a0f6
Michael Wu <wuxiaohua1011@gmail.com>
2018-01-21 11:56:04 -0800

    info window added for markers

--- compete/school_source.js
+++ compete/school_source.js
@@ -1,5 +1,35 @@
 var schools = [
-  ["Acalanes High School", 37.9043022,-122.0999264],
-  ["Alameda Community Learning Center",37.7794615,-122.2894],
-  ["Alameda High School",37.7702805,-122.2859331],
-];
+  ["Acalanes High School", 37.9043022,-122.0999264,"https://www.acalanes.k12.ca.us/acalanes"],
+  ["Alameda Community Learning Center",37.7794615,-122.2894,"http://www.alamedaclc.org/"],
+  ["Alameda High School",37.7702805,-122.2859331,"https://aus-alamedausd-ca.schoolloop.com/"],
+]; // school_name, x-alt, y-alt,school_website
+function add_content(school_name, body, url){
+  console.log(school_name);
+  var contentString = '<div id="content">'+
+            '<div id="siteNotice">'+
+            '</div>'+
+            '<h1 id="firstHeading" class="firstHeading">' + school_name + '</h1>'+
+            '<div id="bodyContent">'+
+            '<p>' + body + '</p>'+
+            '<p>School Website: <a href="'+url+'">'+ url + ' </a> '+
+            '</p>'+
+            '</div>'+
+            '</div>';
+  return contentString;
+}
+
+function createMarker(m, pos, school_name,body, url) {
+    var marker = new google.maps.Marker({
+        position: pos,
+        map: m,  // google.maps.Map
+        title: school_name
+    });
+    google.maps.event.addListener(marker, 'click', function() {
+      var contentString = add_content(school_name,body,url);
+      var infowindow = new google.maps.InfoWindow({
+       content: contentString
+     });
+     infowindow.open(map, marker);
+    });
+    return marker;
+}
