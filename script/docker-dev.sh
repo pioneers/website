7ad14f7c7c5f9e38b60f6621f9b8efcd126c771f
scottnuma <scott.numamoto@gmail.com>
2018-06-30 00:43:27 -0700

    Update docker-dev.sh to account for live reload
    - Yet to actually update the development docker image

--- script/docker-dev.sh
+++ script/docker-dev.sh
@@ -1,5 +1,5 @@
 #! /usr/bin/env bash
-docker run -i --rm -p 4000:4000 -v "$PWD:/srv/jekyll" numascott/jekylldev:latest serve --incremental
+docker run -i --rm -p 4000:4000 -p 35729:35729 -v "$PWD:/srv/jekyll" numascott/jekylldev:latest serve --incremental
 
 # Breaking it down
 
